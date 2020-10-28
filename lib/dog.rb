0934674c4b1d959b2f40330d55f9ef57c6b3e88d
SauvageRene <renegarcia1864@gmail.com>
2020-10-28 17:36:08 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+#sets the name of the person in an instance variable @name
+class Person 
+    def initialize(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+end
+
+class Dog
+    def initialize(name, breed = "Mutt")
+        @name = name
+        @breed = breed
+end
+def name 
+    @name
+end
+def breed=(breed)
+    @breed = breed
+end
+end
